d74c132ad7815edc0cf085ef747a5db0d3b7ec13
Denis <chilikin.den@gmail.com>
2021-12-23 21:41:50 +0300

    Рефактаринг №2

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Input from "../Input";
-import { registrationData } from "../object";
 import "./index.css";
 
 const Main: React.FC = () => {
@@ -10,60 +9,50 @@ const Main: React.FC = () => {
   const [password, setPassword] = useState("");
   const [checkPassword, setCheckPassword] = useState("");
 
-  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-  const changeHandler2 = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSurname(e.target.value);
-  };
-
-  const changeHandler3 = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-  const changeHandler4 = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-  const changeHandler5 = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCheckPassword(e.target.value);
-  };
-  registrationData.name = name;
-  registrationData.surname = surname;
-  registrationData.email = email;
-  registrationData.password = password;
-  registrationData.checkPassword = checkPassword;
-
   return (
     <main className="main">
       <div className="main__container">
         <div className="main__title">Регистрация</div>
         <form className="main__form" action="/">
           <Input
-            onChangeFn={changeHandler}
+            onChangeFn={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setName(e.target.value)
+            }
             inputPlaceholder="Введите имя"
             inputType="text"
           />
           <Input
-            onChangeFn={changeHandler2}
+            onChangeFn={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setSurname(e.target.value)
+            }
             inputPlaceholder="Введите Фамилию"
             inputType="text"
           />
           <Input
-            onChangeFn={changeHandler3}
+            onChangeFn={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
             inputPlaceholder="Ведите Email"
             inputType="email"
           />
           <Input
-            onChangeFn={changeHandler4}
+            onChangeFn={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
             inputPlaceholder="Ведите пароль"
             inputType="password"
           />
           <Input
-            onChangeFn={changeHandler5}
+            onChangeFn={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setCheckPassword(e.target.value)
+            }
             inputPlaceholder="Подтвердите пароль"
             inputType="password"
           />
           <button
-            onClick={() => console.log(registrationData)}
+            onClick={() =>
+              console.log({ name, surname, email, password, checkPassword })
+            }
             className="main__btn"
             type="button">
             Отправить
